6c1fa2ee17dee3ea5f49348138b7b3fac9b3c2f1
Uday Naidu <udaykarri5@gmail.com>
2025-11-05 20:44:16 +0000

    i want something like lucido text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Lato', 'sans-serif'],
+        headline: ['Lato', 'sans-serif'],
         code: ['Source Code Pro', 'monospace'],
       },
       colors: {
